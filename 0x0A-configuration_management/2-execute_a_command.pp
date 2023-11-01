d645b10f6252bc10028bb07640ece7b482be7cfc
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-11-01 12:03:01 -0700

    code test

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Puppet Manifest to Kill a Process Named "killmenow"
-exec { 'kill_killmenow':
-  command     => 'pkill killmenow',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
-  subscribe   => Exec['start_my_process'],
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
